299d66248ea4cee434bda63f42ae83f82c1d124b
Julia <jkar@gmail.com>
2025-04-17 15:24:41 +0200

    Restructure the project directory

--- folder3/file1.py
+++
@@ -1,9 +0,0 @@
-import calendar
-
-def average_days_in_month():
-    total_days = sum(calendar.mdays[1:])  # Sum of days in all months
-    avg_days = total_days / 12  # Average days in a month
-    return avg_days
-
-if __name__ == "__main__":
-    print(f"Average number of days in a month: {average_days_in_month():.2f}")
